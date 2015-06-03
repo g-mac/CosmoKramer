1e6b50ed1ca46f6e3434d68084be63d11b6b1d05
g-mac <simonmayrshofer@hotmail.com>
2015-06-04 01:25:53 +0200

    added: next step of tutorial (conncte to mongo db)

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,17 @@ router.get('/helloworld', function (req, res) {
     res.render('helloworld', {title: 'Hello, Simon!'})
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
 /* GET HTML test page. */
 router.get('/test', function(req, res) { // routes to serve the static HTML files
     res.sendfile(html_dir + 'test.html');
